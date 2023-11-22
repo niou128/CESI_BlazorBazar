07f8f82b8257340856d5370ae172a2252180dc4a
Sébastien <niou128@gmail.com>
2023-11-22 21:43:56 +0100

    feat(webapp):

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebApp;
@@ -9,7 +11,20 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var baseAddress = new Uri("https://localhost:32788");
+var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
+builder.Services.AddScoped(sp => GrpcChannel.ForAddress(baseAddress, new GrpcChannelOptions { HttpHandler = httpHandler }));
+
+builder.Services.AddScoped(services =>
+{
+    var baseAddress = new Uri("https://localhost:32788");
+    var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
+    var grpcChannel = GrpcChannel.ForAddress(baseAddress, new GrpcChannelOptions { HttpHandler = httpHandler });
+    return new OrderServiceClient(grpcChannel);
+});
+
 builder.Services.AddScoped<UserServiceClient>();
 builder.Services.AddScoped<ProductServiceClient>();
+builder.Services.AddScoped<OrderServiceClient>();
 
 await builder.Build().RunAsync();
---
+++ WebApp/Services/OrderServiceClient.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using OrderService;
+
+namespace WebApp.Services
+{
+    public class OrderServiceClient
+    {
+        private readonly Order.OrderClient _client;
+
+        public OrderServiceClient(GrpcChannel channel)
+        {
+            _client = new Order.OrderClient(channel);
+        }
+
+        public async Task<List<OrderReply>> GetOrdersForUser(string userId)
+        {
+            var request = new UserQuery { UserId = userId };
+            try
+            {
+                var reply = await _client.GetOrdersByUserAsync(request);
+                return new List<OrderReply>(reply.Orders);
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine($"gRPC call error: {e.Status.Detail}");
+                return new List<OrderReply>();
+            }
+        }
+
+        public async Task<OrderReply> PlaceOrderAsync(OrderRequest request)
+        {
+            try
+            {
+                return await _client.PlaceOrderAsync(request);
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine($"gRPC call error: {e.Status.Detail}");
+                throw;
+            }
+        }
+    }
+}
