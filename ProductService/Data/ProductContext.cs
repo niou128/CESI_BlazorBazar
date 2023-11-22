7dd8f7e1030a0022918e3720289653f5f8a2beb5
Sébastien <niou128@gmail.com>
2023-11-22 20:55:46 +0100

    feat(productservice): création de la base de données

---
+++ ProductService/Data/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Models;
+
+namespace ProductService.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
