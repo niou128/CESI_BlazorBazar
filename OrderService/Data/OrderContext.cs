ac7e1dfc253db7ee1fa82eac61214ffb43c929ec
Sébastien <niou128@gmail.com>
2023-11-22 21:15:59 +0100

    feat(orderservice): création de la base de données

---
+++ OrderService/Data/OrderContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Models;
+
+namespace OrderService.Data
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+    }
+}
