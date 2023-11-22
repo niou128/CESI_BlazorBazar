e20f27e4bf74db452e6d41fada054e3a0be1d1bb
Sébastien <niou128@gmail.com>
2023-11-22 19:45:12 +0100

    feat(userservice): création de la base de données

---
+++ UserService/Data/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+namespace UserService.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
