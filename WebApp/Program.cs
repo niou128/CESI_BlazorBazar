601d6de75c83c18688166bf6b376c2ac389ed3c3
Sébastien <niou128@gmail.com>
2023-11-22 21:07:23 +0100

    feat(webapp): ajout de la gestion des produits

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<UserServiceClient>();
+builder.Services.AddScoped<ProductServiceClient>();
 
 await builder.Build().RunAsync();
---
+++ WebApp/Services/ProductServiceClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class ProductServiceClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string ProductApiUrl;
+
+        public ProductServiceClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            ProductApiUrl = "https://localhost:32786/Products/";
+        }
+
+        public async Task<Product[]> GetProducts()
+        {
+            var response = await _httpClient.GetAsync($"{ProductApiUrl}");
+            var content = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            return JsonSerializer.Deserialize<Product[]>(content, options);
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            var response = await _httpClient.GetAsync($"{ProductApiUrl}{id}");
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Product>(content);
+        }
+    }
+}
