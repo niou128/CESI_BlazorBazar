ad6838179316f7a5d3272832a455f39f515a8a3b
Sébastien <niou128@gmail.com>
2023-11-22 21:50:10 +0100

    feat(webapp):

---
+++ WebApp/Services/CartService.cs
@@ -0,0 +1,26 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class CartService
+    {
+        private List<Product> _cartItems = new List<Product>();
+
+        public IReadOnlyList<Product> CartItems => _cartItems.AsReadOnly();
+
+        public void AddToCart(Product product)
+        {
+            _cartItems.Add(product);
+            product.Stock--;
+        }
+
+        public void EmptyCart()
+        {
+            foreach (var product in _cartItems)
+            {
+                product.Stock++;
+            }
+            _cartItems.Clear();
+        }
+    }
+}
